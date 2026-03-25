550e28c4a4d45f882e162cb9ae79e1a5cd6c31d7
Me <ryanh41137@gmail.com>
2026-03-25 16:34:22 -0700

    Add meta description for SEO.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,5 +8,6 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Star Wars Weather</title>
+	<meta name="description" content="A weather app which compares the conditions to a planet from Star Wars.">
 </svelte:head>
 {@render children()}
